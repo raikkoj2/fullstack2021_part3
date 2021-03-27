ce5632bff0ca842bda4dec215220ac563e3e577b
raikkoj2 <60006318+raikkoj2@users.noreply.github.com>
2021-03-27 20:36:03 +0200

    finish 3.18

--- index.js
+++ index.js
@@ -30,30 +30,6 @@ app.use(morgan(function (tokens, req, res) {
 )
 
 
-
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-      },
-      {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-      },
-      {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-      },
-      {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-      }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Phonebook</h1>')
 })
