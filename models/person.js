54df7f45a023fdd2ef2d6b6d9f1382368443a460
raikkoj2 <60006318+raikkoj2@users.noreply.github.com>
2021-03-27 23:22:26 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator');
+const uniqueValidator = require('mongoose-unique-validator')
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
+mongoose.connect(url, {
+  useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true,
+})
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -13,28 +16,27 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        unique: true,
-        minlength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        required: true
-    },
+  name: {
+    type: String,
+    unique: true,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+  },
 })
 
-personSchema.plugin(uniqueValidator);
-  
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
 
 module.exports = mongoose.model('Person', personSchema)
--- mongo.js
+++ mongo.js
@@ -1,16 +1,20 @@
+/* eslint-disable no-undef */
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
+// eslint-disable-next-line no-undef
+if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
 
+// eslint-disable-next-line no-undef
 const password = process.argv[2]
 
-const url =
-  `mongodb+srv://raikkoj2:${password}@fullstack.hqu7x.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+const url = `mongodb+srv://raikkoj2:${password}@fullstack.hqu7x.mongodb.net/phonebook-app?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(url, {
+  useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true,
+})
 
 const personSchema = new mongoose.Schema({
   name: String,
@@ -19,36 +23,30 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
+// eslint-disable-next-line no-undef
 if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        console.log('phonebook:')
-        result.forEach(person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close()
+  Person.find({}).then((result) => {
+    console.log('phonebook:')
+    result.forEach((person) => {
+      console.log(person.name, person.number)
     })
-}else if(process.argv.length === 4){
-    console.log('give also the number as an argument')
     mongoose.connection.close()
-    process.exit(1)
-}else{
+  })
+} else if (process.argv.length === 4) {
+  console.log('give also the number as an argument')
+  mongoose.connection.close()
+  process.exit(1)
+} else {
+  const name = process.argv[3]
+  const number = process.argv[4]
 
-    const name = process.argv[3]
-    const number = process.argv[4]
+  const person = new Person({
+    name,
+    number,
+  })
 
-    const person = new Person({
-        name: name,
-        number: number,
-    })
-    
-    person.save().then(response => {
-        console.log(`added ${name} number ${number} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
-
-
-
-
-
-  
